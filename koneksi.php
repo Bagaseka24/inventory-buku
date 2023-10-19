c7503388fa2f308efd490217e42919197d220528
Mochamad Bagas Eka Febrianto <echo.bagaseka424@gmail.com>
2023-10-19 14:05:59 +0700

    lupa tanda ; & ?>

--- koneksi.php
+++ koneksi.php
@@ -7,6 +7,7 @@ $db="inventory_buku";
 
 $kon = mysqli_connect($host, $user, $password, $db);
 if (!$kon) {
-    die("Koneksi gagal: ".mysqli_connect_error())
+    die("Koneksi gagal: ".mysqli_connect_error());
 }
+?>
 
