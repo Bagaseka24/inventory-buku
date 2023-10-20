01786aab5940454ef869069223c614e12dd8a8b3
Mochamad Bagas Eka Febrianto <echo.bagaseka424@gmail.com>
2023-10-20 15:13:49 +0700

    update code index

--- index.php
+++ index.php
@@ -2,74 +2,73 @@
 <html lang="en">
 <head>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/scroller/2.0.1/css/scroller.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container">
-        <br>
-        <h4>Inventory Perpustakaan </h4>
-        <?php
-        include("koneksi.php");
-
-        //mengecek nilai dari method GET dengan nama id_buku
-        if (isset($_GET['idbuku'])) {
-            $idbuku = htmlspecialchars($_GET['idbuku']);
-
-            $sql = "DELETE FROM `buku` WHERE idanggota = '$idbuku'";
-            $result = mysqli_query($konn, $sql);
-
-            if ($result) {
-                header("Location:index.php");
-            } else {
-                echo"<div class='alert alert-danger'> Data gagal dihapus</div>";
-            }
-        }
-        ?>
-
-        <table class="table table-bordered table-hover">
-            <br>
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Nama Buku</th>
-                    <th>Jenis Buku</th>
-                    <th>Tahun Terbit</th>
-                    <th>Pengarang</th>
-                    <th>Penerbit</th>
-                    <th>ISBN</th>
-                    <th colspan="2">Aksi</th>
-                </tr>
-            </thead>
-            <?php
-            include("koneksi.php");
-            $sql = "SELECT * FROM `buku` order by idbuku desc";
-
-            $result = mysqli_query($kon, $sql);
-            $no = 0;
-            while ($data = mysqli_fetch_array($result)) {
-                $no ++;
-
-                ?>
+<div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              INVENTORY BUKU
+            </div>
+            <div class="card-body">
+              <a href="update.php" class="btn btn-md btn-success" style="margin-bottom: 10px">TAMBAH DATA</a>
+              <table class="table table-bordered" id="myTable">
+                <thead>
+                  <tr>
+                    <th scope="col">No.</th>
+                    <th scope="col">Nama Buku</th>
+                    <th scope="col">Kategori Buku</th>
+                    <th scope="col">Tahun Terbit</th>
+                    <th scope="col">Pengarang</th>
+                    <th scope="col">Penerbit</th>
+                    <th scope="col">ISBN</th>
+                    <th scope="col">AKSI</th>
+                  </tr>
+                </thead>
                 <tbody>
-                    <tr>
-                        <td><?php echo $no;?></td>
-                        <td><?php echo $data["namabuku"]; ?></td>
-                        <td><?php echo $data["jenisbuku"]; ?></td>
-                        <td><?php echo $data["tahunterbit"]; ?></td>
-                        <td><?php echo $data["pengarang"]; ?></td>
-                        <td><?php echo $data["penerbit"]; ?></td>
-                        <td><?php echo $data["ISBN"]; ?></td>
-                        <td>
-                            <a href="update.php?idbuku=<?php echo htmlspecialchars($data['idbuku']); ?>" class="btn btn-warning" role="button">Update</a>
-                            <a href="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>?idbuku<? echo $data['idbuku']; ?>)" class="btn btn-warning" role="button">Delete</a>
-                        </td>
-                    </tr>
+                  <?php 
+                      include('koneksi.php');
+                      $no = 1;
+                      $query = mysqli_query($kon,"SELECT * FROM buku");
+                      while($row = mysqli_fetch_array($query)){
+                  ?>
+
+                  <tr>
+                      <td><?php echo $no++ ?></td>
+                      <td><?php echo $row['namabuku'] ?></td>
+                      <td><?php echo $row['jenisbuku'] ?></td>
+                      <td><?php echo $row['tahunterbit'] ?></td>
+                      <td><?php echo $row['pengarang'] ?></td>
+                      <td><?php echo $row['penerbit'] ?></td>
+                      <td><?php echo $row['isbn'] ?></td>
+                      <td class="text-center">
+                        <a href="edit-siswa.php?id=<?php echo $row['id_siswa'] ?>" class="btn btn-sm btn-primary">EDIT</a>
+                        <a href="hapus-siswa.php?id=<?php echo $row['id_siswa'] ?>" class="btn btn-sm btn-danger">HAPUS</a>
+                      </td>
+                  </tr>
+
+                <?php } ?>
                 </tbody>
-                <?php
-            }
-            ?>
-        </table>
-        <a href="create.php" class="btn btn-primary" role="button">Tambah Data</a>
+              </table>
+            </div>
+          </div>
+      </div>
     </div>
-    
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <!-- <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script> -->
+    <script>
+      $(document).ready( function () {
+          $('#myTable').DataTable();
+      } );
+    </script>
 </body>
 </html>
