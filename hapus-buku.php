32a421798428d7ab68442a189ec27da754d50e0e
Mochamad Bagas Eka Febrianto <echo.bagaseka424@gmail.com>
2023-10-20 15:33:28 +0700

    fix variabel query

--- hapus-buku.php
+++ hapus-buku.php
@@ -5,9 +5,9 @@ include('koneksi.php');
 //get id
 $id = $_GET['id'];
 
-$query = "DELETE FROMbuku WHERE idbuku = '$id'";
+$query = "DELETE FROM buku WHERE idbuku = '$id'";
 
-if($connection->query($query)) {
+if($kon->query($query)) {
     header("location: index.php");
 } else {
     echo "DATA GAGAL DIHAPUS!";
